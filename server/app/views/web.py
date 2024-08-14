db9b1918502bd877260e0ea38116033e3aba6abe
杨旸 <yyned2501@vip.qq.com>
2024-08-14 09:17:23 +0800

    修改显示格式

--- server/app/views/web.py
+++ server/app/views/web.py
@@ -11,6 +11,10 @@ app = Blueprint("web", __name__)
 @app.route("/state")
 def state():
     _states = copy.deepcopy(states)
-    _states["next_dt"] = datetime.datetime.fromtimestamp(_states.get("next_time", 0))
-    _states["update_dt"] = datetime.datetime.fromtimestamp(_states.get("update_time", 0))
+    _states["next_ts"] = datetime.datetime.fromtimestamp(
+        _states.get("next_time", 0)
+    ).fromtimestamp("%Y-%m-%d %H:%M:%S")
+    _states["update_ts"] = datetime.datetime.fromtimestamp(
+        _states.get("update_time", 0)
+    ).fromtimestamp("%Y-%m-%d %H:%M:%S")
     return render_template("state.html", states=states)
