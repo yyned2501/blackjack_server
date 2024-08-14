2f53d221553d33f00e54e2f6d10ba9a56085213c
杨旸 <yyned2501@vip.qq.com>
2024-08-14 09:32:46 +0800

    bug fix

--- server/app/views/web.py
+++ server/app/views/web.py
@@ -13,9 +13,9 @@ def state():
     _states = copy.deepcopy(states)
     for _, state in _states.items():
         state["next_ts"] = datetime.datetime.fromtimestamp(
-            _states.get("next_time", 0)
+            state.get("next_time", 0)
         ).strftime("%Y-%m-%d %H:%M:%S")
         state["update_ts"] = datetime.datetime.fromtimestamp(
-            _states.get("update_time", 0)
+            state.get("update_time", 0)
         ).strftime("%Y-%m-%d %H:%M:%S")
     return render_template("state.html", states=states)
