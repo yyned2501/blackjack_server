67841f4baab561a92376b782b359e47b25772cfd
杨旸 <yyned2501@vip.qq.com>
2024-08-14 09:30:40 +0800

    bug fix

--- server/app/views/web.py
+++ server/app/views/web.py
@@ -11,10 +11,11 @@ app = Blueprint("web", __name__)
 @app.route("/state")
 def state():
     _states = copy.deepcopy(states)
-    _states["next_ts"] = datetime.datetime.fromtimestamp(
-        _states.get("next_time", 0)
-    ).strftime("%Y-%m-%d %H:%M:%S")
-    _states["update_ts"] = datetime.datetime.fromtimestamp(
-        _states.get("update_time", 0)
-    ).strftime("%Y-%m-%d %H:%M:%S")
+    for _, state in _states.items():
+        state["next_ts"] = datetime.datetime.fromtimestamp(
+            _states.get("next_time", 0)
+        ).strftime("%Y-%m-%d %H:%M:%S")
+        state["update_ts"] = datetime.datetime.fromtimestamp(
+            _states.get("update_time", 0)
+        ).strftime("%Y-%m-%d %H:%M:%S")
     return render_template("state.html", states=states)
