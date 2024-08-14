54bb75ac454ff917300cbd2b14e4aee22d01d9af
杨旸 <yyned2501@vip.qq.com>
2024-08-14 08:56:57 +0800

    测试web

--- server/app/views/web.py
+++ server/app/views/web.py
@@ -1,14 +1,16 @@
-import json
-import time
+import copy
+import datetime
 
-from flask import render_template, request, Blueprint
+from flask import render_template, Blueprint
+
 from .api import states
-from app.libs import celery_tasks
-from app.init import redis_cli
 
 app = Blueprint("web", __name__)
 
 
 @app.route("/state")
 def state():
+    _states = copy.deepcopy(states)
+    _states["next_dt"] = datetime.datetime.fromtimestamp(_states.get("next_time", 0))
+    _states["update_dt"] = datetime.datetime.fromtimestamp(_states.get("update_time", 0))
     return render_template("state.html", states=states)
