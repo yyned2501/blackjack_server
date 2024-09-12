6a6a34f7f3a7d803246da5155947b10451753836
yyned2501 <yyned2501@vip.qq.com>
2024-09-12 20:16:50 +0800

    修改推送表述

--- server/app/libs/celery_tasks.py
+++ server/app/libs/celery_tasks.py
@@ -10,6 +10,7 @@ def tg_message(content: str):
 
 @celery.task
 def tg_state(state: dict):
+    model = "自助" if state.get("gift_model") else "钓鱼"
     return tgbot.telegram_bot(
-        f'{state.get("userid")}开始钓鱼，点数{state.get("point","未知")}，魔力{state.get("bonus","未知")}，下载量{state.get("downloads","未知")}'
+        f'{state.get("userid")}开始{model}，点数{state.get("point","未知")}，魔力{state.get("bonus","未知")}，下载量{state.get("downloads","未知")}'
     )
